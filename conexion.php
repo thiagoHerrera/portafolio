acdf43565450d951025c83807ed68afb096b2cc8
thiagoherrera <tititomash@gmail.com>
2025-04-07 20:27:17 -0300

    yuhjyuftr

--- conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-        try{
-            $cfg = "mysql:host=localhost;dbname=restaurante;port3306";
-            $cnx = new PDO($cfg, 'root', '');
-            #print 'conexion exitoza';
-        }catch(PDOException $e){
-            print $e->getmessage();
-        }
-?>
--- import-bootstrap.php
+++
@@ -1,3 +0,0 @@
-<link href="./Bootstrap/bootstrap-5.3.3-dist/bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<script src="./Bootstrap/bootstrap-5.3.3-dist/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
-<link rel="stylesheet" href="./Bootstrap/bootstrap-icons-1.11.3/bootstrap-icons-1.11.3/font/bootstrap-icons.min.css">
